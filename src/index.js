3f092db6efa807daf161cfb917d0d833b44dd3e7
Aleksey <alekseymishuk@gmail.com>
2018-09-21 22:00:57 +0300

    implemented check brackets method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketStack = [];
+
+  let index = 0;
+  str = str.split("");
+  while(index < str.length) {
+    const symbol = str[index];
+    const currentBrackets = bracketStack.pop();
+    if (!currentBrackets || currentBrackets[1] !== symbol){
+      const pair = bracketsConfig.find(it => it[0] === symbol);
+      if (!pair) return false;
+      if (currentBrackets) bracketStack.push(currentBrackets);
+      bracketStack.push(pair);
+    }
+    index++;
+  }
+  return bracketStack.length == 0;
 }
